2cdf82e4bec47a9a4c3547f883b1318c934f6435
dir-FY24ogawarin <rin.ogawa.h226@dirbato.co.jp>
2024-04-09 10:58:32 +0900

    途中結果

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,9 +1,9 @@
 package company;
 
-public class Employee {
+public abstract class Employee implements Workable {
     //フィールド
     protected final String name;
-    private final Department department;
+    protected final Department department;
     private final String position;
     private final int employeeId;
     
@@ -25,8 +25,12 @@ public class Employee {
         report(1);
     }
     //会議に参加するメリット
-    public void joinMeeting() {
-        department.meeting();
-        System.out.println("→上記の会議に参加します。部署："+department.getName()+"名前:"+name);
+    public abstract void joinMeeting();
+       
+    
+    //働くメゾッドを実装
+    @Override
+    public void work() {
+        System.out.println("正社員として働きます。名前："+name+slogan);
     }
 }
